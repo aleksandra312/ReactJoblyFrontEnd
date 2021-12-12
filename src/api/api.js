e0c3e8584310af9e74c3bd8984e0620df27df686
aleksandra <oleksandra.klevchuk@gmail.com>
2021-12-12 14:17:52 -0600

    add loginForm.

--- src/api/api.js
+++ src/api/api.js
@@ -43,6 +43,11 @@ class JoblyApi {
     let res = await this.request("jobs", { title });
     return res.jobs;
   }
+
+  static async login(data) {
+    let res = await this.request("auth/token", data, "post");
+    return res.token;
+  }
 }
 
 // for now, put token ("testuser" / "password" on class)
--- src/auth/LoginForm.js
+++ src/auth/LoginForm.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import JoblyApi from "../api/api";
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
-  return <h1>Login Form</h1>;
+  const INITIAL_STATE = { username: "", password: "" };
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState(INITIAL_STATE);
+
+  const handleChange = (evt) => {
+    const { name, value } = evt.target;
+    setFormData((d) => ({ ...d, [name]: value }));
+  };
+
+  async function handleSubmit(evt) {
+    evt.preventDefault();
+    const res = await JoblyApi.login(formData);
+    if (res.success) {
+      navigate("/companies");
+    }
+  }
+
+  return (
+    <div className="LoginForm">
+      <h3>Log In</h3>
+      <form onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+        <label>Password</label>
+        <input
+          type="text"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginForm;
